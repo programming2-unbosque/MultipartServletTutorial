10d7d6ff24b0f79f873fc026249a6cd72048a63e
Fabian Peña <fabiancpl91@gmail.com>
2021-09-30 13:31:18 -0500

    Stable version

---
+++ src/main/java/co/edu/unbosque/multipartservlettutorial/MultipartServlet.java
@@ -0,0 +1,42 @@
+package co.edu.unbosque.multipartservlettutorial;
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "multiPartServlet", value = "/multiPartServlet")
+@MultipartConfig(fileSizeThreshold = 1024 * 1024,
+        maxFileSize = 1024 * 1024 * 5,
+        maxRequestSize = 1024 * 1024 * 5 * 5)
+public class MultipartServlet extends HttpServlet {
+    private String message;
+    private String UPLOAD_DIRECTORY = "uploads";
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        String uploadPath = getServletContext().getRealPath("") + File.separator + UPLOAD_DIRECTORY;
+        File uploadDir = new File(uploadPath);
+        if (!uploadDir.exists()) uploadDir.mkdir();
+
+        String fileName = "";
+        try {
+            for (Part part : request.getParts()) {
+                fileName = part.getSubmittedFileName();
+                part.write(uploadPath + File.separator + fileName);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        response.sendRedirect(request.getContextPath() + "/result.html");
+    }
+
+    public void destroy() {
+    }
+}
